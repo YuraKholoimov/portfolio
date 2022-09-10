5076b1978257afc6e94ad7ca667fd16ac7da332a
Yura Kholoimov <yurakholoimov@ya.ru>
2022-09-10 16:58:14 +0300

    install reactHookForm

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,52 +1,84 @@
-import React from 'react';
+import { TextField } from "@mui/material";
+import { BigButton } from "../UI/BigButton/BigButton";
 import PageTitle from "../UI/PageTitle";
 import s from "./contact.module.css";
-import {Button, TextField} from "@mui/material";
-import {BigButton} from "../UI/BigButton/BigButton";
+import {useForm} from "react-hook-form";
 
 const Contact = () => {
-    const description = "Если у вас есть какое-либо предложение, проект или вы просто хотите поздороваться, пожалуйста, заполните форму ниже, и я отвечу вам в ближайшее время."
-    return (
-        <section id={"contact"}>
-            <div className={s.container}>
-                <PageTitle title={"Связаться"} subTitle={"Контакты"} description={description}/>
-                <div className={s.form}>
-                    <form action="src/Components/contact/Contact">
-                        <div className={s.textField}>
-                            <TextField id="outlined-basic" label="Имя *" variant="outlined"
-                                      sx={{padding: "0px 15px 15px 0px", width: "100%",}}/>
-                            <TextField id="outlined-basic" label="Email*" variant="outlined"
-                                       sx={{padding: "0px 0px 15px 0px", width: "100%"}}/>
-                        </div>
+  const description =
+    `"Если у вас есть какое-либо предложение, проект или вы просто хотите поздороваться,
+     пожалуйста, заполните форму ниже, и я отвечу вам в ближайшее время."`;
+    
+    const {register, handleSubmit,} = useForm();
 
-                            <TextField id="outlined-basic" label="Телефон*" variant="outlined"
-                                       sx={{padding: "0px 0px 15px 0px", width: '100%'}}/>
-                            <TextField
-                                id="outlined-multiline-static"
-                                label="Сообщение"
-                                multiline
-                                rows={6}
-                                sx={{padding: "0px 0px 15px 0px", width: '100%'}}
-                            />
-                        <BigButton>Отправить сообщение</BigButton>
-                    </form>
 
-                </div>
-
-                <div className={s.contact_info}>
-                    <p>Адрес</p>
-                    <h3>Россия, Москва</h3>
-                    <p>Телефон</p>
-                    <h3>
-                        <a href="tel:+79772641610">(+7) 977 264 1610</a>
-                    </h3>
-                    <p>
-                        <a className={s.link_email} href="mailto:yurakholoimov@ya.ru">yurakholoimov@ya.ru</a>
-                    </p>
-                </div>
+    const onSubmit = (data: any) => {
+        console.log(data)
+    }
+    
+  return (
+    <section id={"contact"}>
+      <div className={s.container}>
+        <PageTitle
+          title={"Связаться"}
+          subTitle={"Контакты"}
+          description={description}
+        />
+        <div className={s.form}>
+          <form  onSubmit={handleSubmit(onSubmit)} >
+            <div className={s.textField}>
+              <TextField
+                id="outlined-basic"
+                label="Имя *"
+                variant="outlined"
+                sx={{ padding: "0px 15px 15px 0px", width: "100%" }}
+                {...register("name", {required: true})}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Email*"
+                variant="outlined"
+                sx={{ padding: "0px 0px 15px 0px", width: "100%" }}
+                {...register("email", {required: true})}
+              />
             </div>
-        </section>
-    );
+
+            <TextField
+              id="outlined-basic"
+              label="Телефон*"
+              variant="outlined"
+              sx={{ padding: "0px 0px 15px 0px", width: "100%" }}
+              {...register("phone", {required: true})}
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="Сообщение"
+              multiline
+              rows={6}
+              sx={{ padding: "0px 0px 15px 0px", width: "100%" }}
+              {...register("message", {required: true})}
+            />
+            <BigButton>Отправить сообщение</BigButton>
+            <button type="submit">SEND</button>
+          </form>
+        </div>
+
+        <div className={s.contact_info}>
+          <p>Адрес</p>
+          <h3>Россия, Москва</h3>
+          <p>Телефон</p>
+          <h3>
+            <a href="tel:+79772641610">(+7) 977 264 1610</a>
+          </h3>
+          <p>
+            <a className={s.link_email} href="mailto:yurakholoimov@ya.ru">
+              yurakholoimov@ya.ru
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
